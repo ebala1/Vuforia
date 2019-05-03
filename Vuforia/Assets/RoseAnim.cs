22e2eeaf632c17200bef1231f0ab14ff30077e79
ebala1 <34252455+ebala1@users.noreply.github.com>
2019-05-03 15:13:44 +0200

    Animation Rose

---
+++ Vuforia/Assets/RoseAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseAnim : MonoBehaviour {
+    private Animator Anim;
+	
+	void Start () {
+        Anim = GetComponent<Animator>();
+        Anim.speed = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AnimUp()
+    {
+        Anim.Play("RoseSee", -1, 0f);
+        Anim.speed = 1f;
+    }
+
+    public void AnimRotate()
+    {
+        Anim.Play("RoseRotate", -1, 0f);
+        Anim.speed = 1f;
+    }
+}
